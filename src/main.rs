b86f74f7aeebf33dec3e0df05dcf3e62a6a6d537
Boris Pfahringer <bpnzltd@gmail.com>
2015-02-22 22:56:39 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,12 @@ fn main() {
     rng.shuffle(&mut deck[..]);
     let mut state = State::new(4, deck);
     while !state.is_terminal() {
-        let iters = if state.current_player() == 0 { 10000 } else { 10000 };
-        let move_ = best_move(&mut state.clone(), iters, &mut rng);
+        let move_ = best_move(&mut state.clone(), 500, &mut rng);
         if state.top_card().is_none() {
             println!("\n");
         }
-        println!("{} plays {:?}  [{:?}]",
-            state.current_player(), move_, &state);
+        println!("{} => {:13}  [{:?}]",
+            state.current_player(), format!("{:?}", move_), &state);
         state.apply(move_);
     }
     println!("\n\nWINNER: {}", state.winner());
